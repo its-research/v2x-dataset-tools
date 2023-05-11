913e36382bc33f25e6576b23aaed8ae16c0f98ca
Li Bin <lbin@outlook.com>
2023-05-11 13:59:47 +0800

    rm v2x_i

--- v2x_datasets_tools/rope3d/show_2d3d_box.py
+++ v2x_datasets_tools/rope3d/show_2d3d_box.py
@@ -1,17 +1,14 @@
-"""
-File: show_2d3d_box.py
-author: yexiaoqing, liyingying
-"""
+import glob as gb
+import math
 import os
+import sys
+
+import config
 import cv2
 import numpy as np
-import math
-import sys
-import config
-import glob as gb
-import pdb
 import yaml
 from pyquaternion import Quaternion
+
 # stop python from writing so much bytecode
 sys.dont_write_bytecode = True
 sys.path.append(os.getcwd())
@@ -19,11 +16,31 @@ np.set_printoptions(suppress=True)
 
 
 class Data:
-    """ class Data """
-    def __init__(self, obj_type="unset", truncation=-1, occlusion=-1, \
-                 obs_angle=-10, x1=-1, y1=-1, x2=-1, y2=-1, w=-1, h=-1, l=-1, \
-                 X=-1000, Y=-1000, Z=-1000, yaw=-10, score=-1000, detect_id=-1, \
-                 vx=0, vy=0, vz=0):
+    """class Data"""
+
+    def __init__(
+        self,
+        obj_type="unset",
+        truncation=-1,
+        occlusion=-1,
+        obs_angle=-10,
+        x1=-1,
+        y1=-1,
+        x2=-1,
+        y2=-1,
+        w=-1,
+        h=-1,
+        l=-1,
+        X=-1000,
+        Y=-1000,
+        Z=-1000,
+        yaw=-10,
+        score=-1000,
+        detect_id=-1,
+        vx=0,
+        vy=0,
+        vz=0,
+    ):
         """init object data"""
         self.obj_type = obj_type
         self.truncation = truncation
@@ -49,21 +66,22 @@ class Data:
         self.detect_id = detect_id
 
     def __str__(self):
-        """ str """
+        """str"""
         attrs = vars(self)
-        return '\n'.join("%s: %s" % item for item in attrs.items())
+        return "\n".join("%s: %s" % item for item in attrs.items())
 
 
-def progress(count, total, status=''):
-    """ update a prograss bar"""
+def progress(count, total, status=""):
+    """update a prograss bar"""
     bar_len = 60
     filled_len = int(round(bar_len * count / float(total)))
 
     percents = round(100.0 * count / float(total), 1)
-    bar = '=' * filled_len + '>' + '-' * (bar_len - filled_len)
+    bar = "=" * filled_len + ">" + "-" * (bar_len - filled_len)
 
-    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.write("[%s] %s%s ...%s\r" % (bar, percents, "%", status))
     sys.stdout.flush()
+
 
 def read_kitti_cal(calfile):
     """
@@ -72,12 +90,12 @@ def read_kitti_cal(calfile):
     Args:
         calfile (str): path to single calibration file
     """
-    text_file = open(calfile, 'r')
+    text_file = open(calfile, "r")
     for line in text_file:
-        parsed = line.split('\n')[0].split(' ')
+        parsed = line.split("\n")[0].split(" ")
         # bbGt annotation in text format of:
         # cls x y w h occ x y w h ign ang
-        if parsed is not None and parsed[0] == 'P2:':
+        if parsed is not None and parsed[0] == "P2:":
             p2 = np.zeros([4, 4], dtype=float)
             p2[0, 0] = parsed[1]
             p2[0, 1] = parsed[2]
@@ -113,9 +131,7 @@ def project_3d(p2, x3d, y3d, z3d, w3d, h3d, l3d, ry3d, de_norm):
     """
 
     # compute rotational matrix around yaw axis
-    R = np.array([[+math.cos(ry3d), 0, +math.sin(ry3d)],
-                  [0, 1, 0],
-                  [-math.sin(ry3d), 0, +math.cos(ry3d)]])
+    R = np.array([[+math.cos(ry3d), 0, +math.sin(ry3d)], [0, 1, 0], [-math.sin(ry3d), 0, +math.cos(ry3d)]])
 
     # 3D bounding box corners
     x_corners = np.array([0, l3d, l3d, l3d, l3d, 0, 0, 0])
@@ -131,9 +147,7 @@ def project_3d(p2, x3d, y3d, z3d, w3d, h3d, l3d, ry3d, de_norm):
 
     # rotate
     corners_3d = R.dot(corners_3d)
-    R2 = np.array([[1, 0, 0],
-                   [0, -de_norm[1], +de_norm[2]],
-                   [0, -de_norm[2], -de_norm[1]]])
+    R2 = np.array([[1, 0, 0], [0, -de_norm[1], +de_norm[2]], [0, -de_norm[2], -de_norm[1]]])
     corners_3d = R2.dot(corners_3d)
     # translate
     corners_3d += np.array([x3d, y3d, z3d]).reshape((3, 1))
@@ -162,8 +176,7 @@ def load_detect_data(filename):
             fields = line.split(" ")
             t_data = Data()
             # get fields from table
-            t_data.obj_type = fields[
-                0].lower()  # object type [car, pedestrian, cyclist, ...]
+            t_data.obj_type = fields[0].lower()  # object type [car, pedestrian, cyclist, ...]
             t_data.truncation = float(fields[1])  # truncation [0..1]
             t_data.occlusion = int(float(fields[2]))  # occlusion  [0,1,2]
             t_data.obs_angle = float(fields[3])  # observation angle [rad]
@@ -179,9 +192,9 @@ def load_detect_data(filename):
             t_data.Z = float(fields[13])  # Z [m]
             t_data.yaw = float(fields[14])  # yaw angle [rad]
             if len(fields) >= 16:
-              t_data.score = float(fields[15])  # detection score
+                t_data.score = float(fields[15])  # detection score
             else:
-              t_data.score = 1
+                t_data.score = 1
             t_data.detect_id = index
             data.append(t_data)
             index = index + 1
@@ -195,9 +208,9 @@ def load_denorm_data(denormfile):
     Args:
         denormfile (str): path to single de_norm file
     """
-    text_file = open(denormfile, 'r')
+    text_file = open(denormfile, "r")
     for line in text_file:
-        parsed = line.split('\n')[0].split(' ')
+        parsed = line.split("\n")[0].split(" ")
         if parsed is not None and len(parsed) > 3:
             de_norm = []
             de_norm.append(float(parsed[0]))
@@ -224,16 +237,15 @@ def project_3d_ground(p2, de_bottom_center, w3d, h3d, l3d, ry3d, de_norm, c2g_tr
         de_norm: [a,b,c] denotes the ground equation plane
         c2g_trans: camera_to_ground translation
     """
-    de_bottom_center_in_ground = c2g_trans.dot(de_bottom_center) #convert de_bottom_center in Camera coord into Ground coord
+    de_bottom_center_in_ground = c2g_trans.dot(de_bottom_center)  # convert de_bottom_center in Camera coord into Ground coord
     bottom_center_ground = np.array(de_bottom_center_in_ground)
     bottom_center_ground = bottom_center_ground.reshape((3, 1))
     theta = np.matrix([math.cos(ry3d), 0, -math.sin(ry3d)]).reshape(3, 1)
-    theta0 = c2g_trans[:3, :3] * theta #first column
+    theta0 = c2g_trans[:3, :3] * theta  # first column
     yaw_world_res = math.atan2(theta0[1], theta0[0])
     g2c_trans = np.linalg.inv(c2g_trans)
 
-    verts3d = get_camera_3d_8points_g2c(w3d, h3d, l3d, yaw_world_res,
-        bottom_center_ground, g2c_trans, p2, isCenter=False)
+    verts3d = get_camera_3d_8points_g2c(w3d, h3d, l3d, yaw_world_res, bottom_center_ground, g2c_trans, p2, isCenter=False)
     verts3d = np.array(verts3d)
     return verts3d
 
@@ -254,13 +266,12 @@ def project_3d_world(p2, de_center_in_world, w3d, h3d, l3d, ry3d, camera2world):
         ry3d: rotation w.r.t y-axis
         camera2world: camera_to_world translation
     """
-    center_world = np.array(de_center_in_world) #bottom center in world
+    center_world = np.array(de_center_in_world)  # bottom center in world
     theta = np.matrix([math.cos(ry3d), 0, -math.sin(ry3d)]).reshape(3, 1)
-    theta0 = camera2world[:3, :3] * theta  #first column
+    theta0 = camera2world[:3, :3] * theta  # first column
     world2camera = np.linalg.inv(camera2world)
     yaw_world_res = math.atan2(theta0[1], theta0[0])
-    verts3d = get_camera_3d_8points_g2c(w3d, h3d, l3d,
-        yaw_world_res, center_world[:3, :], world2camera, p2, isCenter=False)
+    verts3d = get_camera_3d_8points_g2c(w3d, h3d, l3d, yaw_world_res, center_world[:3, :], world2camera, p2, isCenter=False)
 
     verts3d = np.array(verts3d)
     return verts3d
@@ -272,35 +283,33 @@ def compute_c2g_trans(de_norm):
     author: yexiaoqing
     function: compute the camera to ground transformation (only rotation matrix)
     """
-    #pdb.set_trace()
-    ground_z_axis = de_norm 
+    # pdb.set_trace()
+    ground_z_axis = de_norm
     cam_xaxis = np.array([1.0, 0.0, 0.0])
     ground_x_axis = cam_xaxis - cam_xaxis.dot(ground_z_axis) * ground_z_axis
     ground_x_axis = ground_x_axis / np.linalg.norm(ground_x_axis)
     ground_y_axis = np.cross(ground_z_axis, ground_x_axis)
     ground_y_axis = ground_y_axis / np.linalg.norm(ground_y_axis)
-    c2g_trans = np.vstack([ground_x_axis, ground_y_axis, ground_z_axis]) #(3, 3)
+    c2g_trans = np.vstack([ground_x_axis, ground_y_axis, ground_z_axis])  # (3, 3)
     return c2g_trans
 
 
 def read_kitti_ext(extfile):
     """read extrin"""
-    text_file = open(extfile, 'r')
+    text_file = open(extfile, "r")
     cont = text_file.read()
     x = yaml.safe_load(cont)
-    r = x['transform']['rotation']
-    t = x['transform']['translation']
-    q = Quaternion(r['w'], r['x'], r['y'], r['z'])
+    r = x["transform"]["rotation"]
+    t = x["transform"]["translation"]
+    q = Quaternion(r["w"], r["x"], r["y"], r["z"])
     m = q.rotation_matrix
     m = np.matrix(m).reshape((3, 3))
-    t = np.matrix([t['x'], t['y'], t['z']]).T
+    t = np.matrix([t["x"], t["y"], t["z"]]).T
     p1 = np.vstack((np.hstack((m, t)), np.array([0, 0, 0, 1])))
     return np.array(p1.I)
 
 
-def get_camera_3d_8points_g2c(w3d, h3d, l3d, yaw_ground, center_ground,
-                          g2c_trans, p2,
-                          isCenter=True):
+def get_camera_3d_8points_g2c(w3d, h3d, l3d, yaw_ground, center_ground, g2c_trans, p2, isCenter=True):
     """
     function: projection 3D to 2D
     w3d: width of object
@@ -314,31 +323,33 @@ def get_camera_3d_8points_g2c(w3d, h3d, l3d, yaw_ground, center_ground,
         1: center,
         0: bottom
     """
-    ground_r = np.matrix([[math.cos(yaw_ground), -math.sin(yaw_ground), 0],
-                         [math.sin(yaw_ground), math.cos(yaw_ground), 0],
-                         [0, 0, 1]])
-    #l, w, h = obj_size
+    ground_r = np.matrix([[math.cos(yaw_ground), -math.sin(yaw_ground), 0], [math.sin(yaw_ground), math.cos(yaw_ground), 0], [0, 0, 1]])
+    # l, w, h = obj_size
     w = w3d
     l = l3d
     h = h3d
 
     if isCenter:
-        corners_3d_ground = np.matrix([[l / 2, l / 2, -l / 2, -l / 2, l / 2, l / 2, -l / 2, -l / 2],
-                                  [w / 2, -w / 2, -w / 2, w / 2, w / 2, -w / 2, -w / 2, w / 2],
-                                  [-h / 2, -h / 2, -h / 2, -h / 2, h / 2, h / 2, h / 2, h / 2]])
-    else:#bottom center, ground: z axis is up
-        corners_3d_ground = np.matrix([[l / 2, l / 2, -l / 2, -l / 2, l / 2, l / 2, -l / 2, -l / 2],
-                                  [w / 2, -w / 2, -w / 2, w / 2, w / 2, -w / 2, -w / 2, w / 2],
-                                  [0, 0, 0, 0, h, h, h, h]])
+        corners_3d_ground = np.matrix(
+            [
+                [l / 2, l / 2, -l / 2, -l / 2, l / 2, l / 2, -l / 2, -l / 2],
+                [w / 2, -w / 2, -w / 2, w / 2, w / 2, -w / 2, -w / 2, w / 2],
+                [-h / 2, -h / 2, -h / 2, -h / 2, h / 2, h / 2, h / 2, h / 2],
+            ]
+        )
+    else:  # bottom center, ground: z axis is up
+        corners_3d_ground = np.matrix(
+            [[l / 2, l / 2, -l / 2, -l / 2, l / 2, l / 2, -l / 2, -l / 2], [w / 2, -w / 2, -w / 2, w / 2, w / 2, -w / 2, -w / 2, w / 2], [0, 0, 0, 0, h, h, h, h]]
+        )
 
-    corners_3d_ground = np.matrix(ground_r) * np.matrix(corners_3d_ground) + np.matrix(center_ground) #[3, 8]
+    corners_3d_ground = np.matrix(ground_r) * np.matrix(corners_3d_ground) + np.matrix(center_ground)  # [3, 8]
 
-    if g2c_trans.shape[0] == 4: #world2camera transformation
+    if g2c_trans.shape[0] == 4:  # world2camera transformation
         ones = np.ones(8).reshape(1, 8).tolist()
         corners_3d_cam = g2c_trans * np.matrix(corners_3d_ground.tolist() + ones)
         corners_3d_cam = corners_3d_cam[:3, :]
-    else:  #only consider the rotation
-        corners_3d_cam = np.matrix(g2c_trans) * corners_3d_ground #[3, 8]
+    else:  # only consider the rotation
+        corners_3d_cam = np.matrix(g2c_trans) * corners_3d_ground  # [3, 8]
 
     pt = p2[:3, :3] * corners_3d_cam
     corners_2d = pt / pt[2]
@@ -354,12 +365,11 @@ def get_camera_3d_8points_g2c(w3d, h3d, l3d, yaw_ground, center_ground,
     return corners_2d
 
 
-
-def show_box_with_roll(name_list, thresh=0.5, projectMethod='Ground'):
+def show_box_with_roll(name_list, thresh=0.5, projectMethod="Ground"):
     """show 2d box and 3d box
-        yexiaoqing modified
-        # 'Ground': using the ground to camera transformation (denorm: the ground plane equation)
-        # 'World': using the extrinsics (world to camera transformation)
+    yexiaoqing modified
+    # 'Ground': using the ground to camera transformation (denorm: the ground plane equation)
+    # 'World': using the extrinsics (world to camera transformation)
     """
     image_root = config.image_dir
     label_dir = config.label_dir
@@ -370,29 +380,29 @@ def show_box_with_roll(name_list, thresh=0.5, projectMethod='Ground'):
     denorm_dir = config.denorm_dir
     use_denorm = False
     use_extrinsic = False
-    if projectMethod == 'Ground':
+    if projectMethod == "Ground":
         use_denorm = True
-    elif projectMethod == 'World':
+    elif projectMethod == "World":
         use_extrinsic = True
-    
+
     for i, name in enumerate(name_list):
         img_path = os.path.join(image_root, name)
         name = name.strip()
-        name = name.split('/')
-        name = name[-1].split('.')[0]
+        name = name.split("/")
+        name = name[-1].split(".")[0]
         progress(i, len(name_list))
-        detection_file = os.path.join(label_dir, '%s.txt' % (name))
+        detection_file = os.path.join(label_dir, "%s.txt" % (name))
         result = load_detect_data(detection_file)
         if use_denorm:
-            denorm_file = os.path.join(denorm_dir, '%s.txt' % (name))
-            de_norm = load_denorm_data(denorm_file)  #[ax+by+cz+d=0]
+            denorm_file = os.path.join(denorm_dir, "%s.txt" % (name))
+            de_norm = load_denorm_data(denorm_file)  # [ax+by+cz+d=0]
             c2g_trans = compute_c2g_trans(de_norm)
         if use_extrinsic:
-            extrinsic_file = os.path.join(extrinsics_dir, '%s.yaml' % (name))
+            extrinsic_file = os.path.join(extrinsics_dir, "%s.yaml" % (name))
             world2camera = read_kitti_ext(extrinsic_file).reshape((4, 4))
             camera2world = np.linalg.inv(world2camera).reshape(4, 4)
-        
-        calfile = os.path.join(cal_dir, '%s.txt' % (name))
+
+        calfile = os.path.join(cal_dir, "%s.txt" % (name))
         p2 = read_kitti_cal(calfile)
         img = cv2.imread(img_path)
         h, w, c = img.shape
@@ -404,15 +414,14 @@ def show_box_with_roll(name_list, thresh=0.5, projectMethod='Ground'):
             if t.obj_type not in config.color_list.keys():
                 continue
             color_type = config.color_list[t.obj_type]
-            cv2.rectangle(img, (t.x1, t.y1), (t.x2, t.y2),
-                          (255, 255, 255), 1)
-            if t.w <= 0.05 and t.l <= 0.05 and t.h <= 0.05: #invalid annotation
+            cv2.rectangle(img, (t.x1, t.y1), (t.x2, t.y2), (255, 255, 255), 1)
+            if t.w <= 0.05 and t.l <= 0.05 and t.h <= 0.05:  # invalid annotation
                 continue
 
             cam_bottom_center = [t.X, t.Y, t.Z]  # bottom center in Camera coordinate
             if use_extrinsic:
                 bottom_center_in_world = camera2world * np.matrix(cam_bottom_center + [1.0]).T
-                verts3d = project_3d_world(p2, bottom_center_in_world, t.w, t.h, t.l, t.yaw, camera2world) 
+                verts3d = project_3d_world(p2, bottom_center_in_world, t.w, t.h, t.l, t.yaw, camera2world)
             if use_denorm:
                 verts3d = project_3d_ground(p2, np.array(cam_bottom_center), t.w, t.h, t.l, t.yaw, de_norm, c2g_trans)
 
@@ -433,25 +442,25 @@ def show_box_with_roll(name_list, thresh=0.5, projectMethod='Ground'):
             cv2.line(img, tuple(verts3d[1]), tuple(verts3d[5]), color_type, 2)
             cv2.line(img, tuple(verts3d[0]), tuple(verts3d[4]), color_type, 2)
             cv2.line(img, tuple(verts3d[2]), tuple(verts3d[6]), color_type, 2)
-        cv2.imwrite('%s/%s.jpg' % (out_dir, name), img)
+        cv2.imwrite("%s/%s.jpg" % (out_dir, name), img)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if config.val_list is None:
         name_list = gb.glob(config.image_dir + "/*")
     else:
-        val_part_list  = open(config.val_list).readlines()
+        val_part_list = open(config.val_list).readlines()
         name_list = []
         for name in val_part_list:
-            name_list.append(name.split('\n')[0] + '.jpg')
+            name_list.append(name.split("\n")[0] + ".jpg")
         name_list.sort()
 
     if not os.path.isdir(config.out_box_dir):
-      os.makedirs(config.out_box_dir)
-   
-    #-----------------------------------------------------------------------------------------------------
+        os.makedirs(config.out_box_dir)
+
+    # -----------------------------------------------------------------------------------------------------
     # --------------Two approaches can be adopted for projection and visualization------------------------
     # 'Ground': using the ground to camera transformation (denorm: the ground plane equation), default
     # 'World': using the extrinsics (world to camera transformation)
     # show_box_with_roll(name_list, projectMethod='Ground')
-    show_box_with_roll(name_list, projectMethod='World')
+    show_box_with_roll(name_list, projectMethod="World")
